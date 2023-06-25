bf1e277345f98a43b3f445a975116ec09e320c81
t-skgm <skgm0301@gmail.com>
2023-06-25 13:45:18 +0900

    connect to db

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,9 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
 declare global {
-  var prisma: PrismaClient | undefined
+  var prisma: PrismaClient | undefined;
 }
 
-const prisma = global.prisma || new PrismaClient()
+export const prisma = global.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV === 'development') global.prisma = prisma
-
-export default prisma
+if (process.env.NODE_ENV === "development") global.prisma = prisma;
