8458044a295bc57d41abf512be4324eca9c93368
t-skgm <skgm0301@gmail.com>
2023-06-25 19:39:44 +0900

    fix

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { prisma } from '../lib/prisma'
+// import { prisma } from '../lib/prisma'
 
 async function main() {
   const response = await Promise.all([
@@ -37,11 +37,11 @@ async function main() {
 }
 
 main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async e => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+// .then(async () => {
+//   await prisma.$disconnect()
+// })
+// .catch(async e => {
+//   console.error(e)
+//   await prisma.$disconnect()
+//   process.exit(1)
+// })
