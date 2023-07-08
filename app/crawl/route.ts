ed81d1a172db932519147706e2e49ed6b965fc70
t-skgm <skgm0301@gmail.com>
2023-07-08 22:32:41 +0900

    fix

--- app/crawl/route.ts
+++ app/crawl/route.ts
@@ -1,6 +1,9 @@
 import { NextResponse } from 'next/server'
 import { crawl } from '@/lib/crawl'
+import { AppConfig } from 'next/dist/build/utils'
 import { saveItems } from '@/lib/saveItems'
+
+export const dynamic: AppConfig['dynamic'] = 'force-dynamic'
 
 export async function GET(request: Request) {
   const reqUrl = new URL(request.url)
