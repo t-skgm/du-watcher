6bb69dc94250fada565a358118378c6bcf2ae219
t-skgm <skgm0301@gmail.com>
2024-04-04 13:37:01 +0900

    fix: eslint

--- code/lib/crawler/parser/parseNextPageUrl.test.ts
+++ code/lib/crawler/parser/parseNextPageUrl.test.ts
@@ -1,6 +1,6 @@
-import { beforeAll, describe, expect, test } from 'vitest'
-import { readFileSync } from 'fs'
-import { resolve } from 'path'
+import { beforeAll, describe, expect, test } from 'bun:test'
+import { readFileSync } from 'node:fs'
+import { resolve } from 'node:path'
 import { parseNextPageUrl } from './parseNextPageUrl'
 
 describe('parseNextPageUrl()', () => {
