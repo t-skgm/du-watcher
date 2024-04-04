31a9087e2408d234e17b88b3a2b8b895239ec955
t-skgm <skgm0301@gmail.com>
2024-04-04 12:52:15 +0900

    fix: delete next & prisma

--- app/pages/[pageId]/page.tsx
+++
@@ -1,9 +0,0 @@
-type PageDetailProps = {
-  params: {
-    pageId: string
-  }
-}
-
-export default function PagesDetail({ params }: PageDetailProps) {
-  return <p>Detail: {params.pageId}</p>
-}
--- components/feature/CreatePageButton.tsx
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link'
-
-export const CreatePageButton = () => {
-  return (
-    <Link href={`/pages/new`} className="text-primary-700 p-2 mb-4 bg-blue-100 border-blue-300 border-2 rounded-lg">
-      New
-    </Link>
-  )
-}
